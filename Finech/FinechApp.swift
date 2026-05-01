df7ff109a34cc4418dd8b94b7aa016440b7be0dd
Dagosh <danilreal25@gmail.com>
2026-05-01 10:35:08 +0300

    Initial Commit

---
+++ Finech/FinechApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinechApp.swift
+//  Finech
+//
+//  Created by Dagosh on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
